9057e43c1020930d4265a378dabb5a2be2e38948
Fabián Lizama <fabian.lizama@usach.cl>
2024-04-04 14:10:54 -0300

    api: QuantityDiscountRepository fix

--- api/src/main/java/com/example/autofix/repositories/QuantityDiscountRepository.java
+++ api/src/main/java/com/example/autofix/repositories/QuantityDiscountRepository.java
@@ -1,8 +1,10 @@
 package com.example.autofix.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.autofix.entities.QuantityDiscountEntity;
 
 @Repository
-public class QuantityRepository {
+public interface QuantityDiscountRepository extends JpaRepository<QuantityDiscountEntity, Long>{
 
 }
