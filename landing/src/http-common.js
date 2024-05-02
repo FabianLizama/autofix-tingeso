89bcd9de9e9fd15c050862b70fc9bea9e07ae395
Fabián Lizama <fabian.lizama@usach.cl>
2024-05-02 13:02:16 -0400

    landing axios connection fix

--- landing/src/http-common.js
+++ landing/src/http-common.js
@@ -1,13 +1,12 @@
 import axios from "axios";
-
-const backendServer = import.meta.env.VITE_PAYROLL_BACKEND_SERVER;
-const backendPort = import.meta.env.VITE_PAYROLL_BACKEND_PORT;
+const backendServer = import.meta.env.VITE_BACKEND_SERVER;
+const backendPort = import.meta.env.VITE_BACKEND_PORT;
 
 console.log(backendServer);
 console.log(backendPort);
 
 export default axios.create({
-  baseURL: `http://localhost:8079`,
+  baseURL: `http://${backendServer}:${backendPort}`,
   headers: {
     "Content-Type": "application/json",
   },
